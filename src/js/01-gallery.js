16c7f44c7764aab8d1de5390aabadf63f22e9bb4
vitasika <vitasika1@gmail.com>
2022-02-09 23:36:46 +0200

    003

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,14 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-//Находим элемент div.gallery
-const galleryEl = document.querySelector('.gallery');
-
-
-
-
-
-
 
 console.log(galleryItems);
-console.log(galleryEl);
