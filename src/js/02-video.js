6101d62c2adf958e924534473abc358b313f3f84
vitasika <vitasika1@gmail.com>
2022-02-12 00:21:11 +0200

    проба 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,21 @@
-// Найти строку в html по тегу iframe
+// Импортирует плеер в этот фаил js
 import Player from '@vimeo/player';
 
 
 // Найти строку в html по тегу iframe
 const iframeEl = document.querySelector('iframe');
-//console.log(iframeEl);
+console.log(iframeEl);
+console.dir(iframeEl);
 
-const player = new Player(iframeEl);
+// const player = new Player(iframeEl);
 
-console.log(player);
 
-// const player = new Player('handstick', {
-//     id: 19231868,
+
+// const player = new Player(iframeEl, {
+//     id: vimeo-player,
 //     width: 640
 // });
-
+// console.log(player);
 // player.on('play', function() {
 //     console.log('played the video!');
 // });
