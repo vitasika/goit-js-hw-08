d087b66360efdf09666c8ea16d74de69a340feca
vitasika <vitasika1@gmail.com>
2022-02-11 23:57:34 +0200

    что то получается - или нет

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,20 @@
 // Найти строку в html по тегу iframe
+import Player from '@vimeo/player';
+
+
+// Найти строку в html по тегу iframe
 const iframeEl = document.querySelector('iframe');
-console.log(iframeEl);
+//console.log(iframeEl);
+
+const player = new Player(iframeEl);
+
+console.log(player);
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
